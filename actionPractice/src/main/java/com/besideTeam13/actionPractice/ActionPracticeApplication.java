ec84ea443a458b1830cae65bc50fb876f8c099d6
Hyesooo <hyesookim95@gmail.com>
2023-02-22 08:09:02 +0900

    feat: springBoot Start!

---
+++ actionPractice/src/main/java/com/besideTeam13/actionPractice/ActionPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.besideTeam13.actionPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionPracticeApplication.class, args);
+	}
+
+}
---
+++ actionPractice/src/test/java/com/besideTeam13/actionPractice/ActionPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.besideTeam13.actionPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
